fdcc28bab5e02f09c906caca92cf6db8ca913734
Grenui92 <107549723+Grenui92@users.noreply.github.com>
2022-10-31 16:56:23 +0300

    Функция вывода данных и исключения для субботы и воскресенья выведены в две отдельные функции.

--- Autocheck/PCHw08/Homework/main.py
+++ Autocheck/PCHw08/Homework/main.py
@@ -1,38 +1,48 @@
 from datetime import datetime, timedelta
-def main(employee: dict):
+def main(employee: list):
 
     result = {}
 
-    for i in employee:
-        employee_birthday = i['birthday']  # Берем дататайм обьект конкретного сотрудника (день рождения)
+    for person in employee:
+        employee_birthday = person['birthday']  # Берем дататайм обьект конкретного сотрудника (день рождения)
         days_dif = (employee_birthday - datetime.now()).days  # Находим разницу ДР сотрудника и текущей даты, потому что нам же нужны только те у кого ДР в ближайшие 7 дней
 
-        if days_dif >= 7 or -1 > days_dif:  # Прерываем данную итерацию, если ДР нам не подходит
+        #Отсеиваем сотрудников у которых ДР не попадает в ближайшие 7 дней
+        if days_dif >= 7 or -1 > days_dif:
             continue
-        if employee_birthday.strftime('%A') in ('Saturday', 'Sunday'):
-            while employee_birthday.strftime('%A') != "Monday": #увеличиваем дату пока она не выпадет на ближайший понедельник
-                employee_birthday += timedelta(days=1)
-            result.setdefault((employee_birthday.strftime("%A"), employee_birthday.date()), []).append(i['name'])
-            continue
+        #Проверяем на ДР в субботу или воскресенье
+        if employee_birthday.strftime("%A") in ('Saturday', 'Sunday'):
+            employee_birthday = from_sturday_and_sunday_to_monday(employee_birthday=employee_birthday)
 
-        result.setdefault((employee_birthday.strftime("%A"), employee_birthday.date()), []).append(i['name'])
+        result.setdefault((employee_birthday.strftime("%A"), employee_birthday.date()), []).append(person['name'])
+    show_days_to_be_blessed(result=result)
 
+
+def from_sturday_and_sunday_to_monday(employee_birthday):
+    if employee_birthday.strftime('%A') == 'Saturday':
+        employee_birthday += timedelta(days=2)
+        return employee_birthday
+    elif employee_birthday.strftime('%A') == 'Sunday':
+        employee_birthday += timedelta(days=1)
+        return employee_birthday
+
+def show_days_to_be_blessed(result):
     for k, v in sorted(result.items(), key=lambda x: x[0][1]):
         print('Who needs to be blessed with a birthday on {}, {}: {}'.format(*k, ', '.join(v)))
 
 
 
-# test_dicts = [{'name': 'Jhon', 'birthday': datetime(year=2022, month=11, day=6)},
-#               {'name': 'Peter', 'birthday': datetime(year=2022, month=11, day=5)},
-#               {'name': 'Roland', 'birthday': datetime(year=2022, month=11, day=8)},
-#               {'name': 'Kate', 'birthday': datetime(year=2022, month=11, day=1)},
-#               {'name': 'Lili', 'birthday': datetime(year=2022, month=11, day=5)},
-#               {'name': 'Rim', 'birthday': datetime(year=2022, month=12, day=8)},
-#               {'name': 'Kiti', 'birthday': datetime(year=2022, month=11, day=2)},
-#               {'name': 'Toma', 'birthday': datetime(year=2022, month=10, day=31)},
-#               {'name': 'Saha', 'birthday': datetime(year=2022, month=11, day=1)},
-#               {'name': 'Fila', 'birthday': datetime(year=2022, month=11, day=4)},
-#               {'name': 'Sreda', 'birthday': datetime(year=2022, month=11, day=3)}]
+test_dicts = [{'name': 'Jhon', 'birthday': datetime(year=2022, month=11, day=6)},
+              {'name': 'Peter', 'birthday': datetime(year=2022, month=11, day=5)},
+              {'name': 'Roland', 'birthday': datetime(year=2022, month=11, day=8)},
+              {'name': 'Kate', 'birthday': datetime(year=2022, month=11, day=1)},
+              {'name': 'Lili', 'birthday': datetime(year=2022, month=11, day=5)},
+              {'name': 'Rim', 'birthday': datetime(year=2022, month=10, day=31)},
+              {'name': 'Kiti', 'birthday': datetime(year=2022, month=11, day=2)},
+              {'name': 'Toma', 'birthday': datetime(year=2022, month=10, day=31)},
+              {'name': 'Saha', 'birthday': datetime(year=2022, month=11, day=1)},
+              {'name': 'Fila', 'birthday': datetime(year=2022, month=11, day=4)},
+              {'name': 'Sreda', 'birthday': datetime(year=2022, month=11, day=3)}]
 
 
 if __name__ == "__main__":
