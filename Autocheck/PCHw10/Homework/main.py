31bd3c2ed87644423b7ad472782b9c3d1b4fff36
Grenui92 <107549723+Grenui92@users.noreply.github.com>
2022-11-02 17:08:37 +0200

    Изменения согласно изменениям в классах

--- Autocheck/PCHw10/Homework/main.py
+++ Autocheck/PCHw10/Homework/main.py
@@ -17,53 +17,48 @@ def input_error(func):
 
 def main():
     print("Команды, которые вы можете использовать: (X / Y в указанных команда = место для ввода пользователя)\n"
-          "'create contact X': Создает нового пользователя X.\n"
-          "'choice contact X': Выбрать пользователя из уже существующих.\n"
-          "'add X Y': Добавляет к записи пользователя номер телефона X(указывает на предназначение по типу 'home/job/own') Y(сам номер телефона).\n"
-          "'change X Y': Изменяет у записи пользователя номер телефона X(указывает на предназначение по типу 'home/job/own') Y(сам номер телефона).\n"
-          "'save': сохраняет текущий контакт в телефонную книгу.\n"
-          "'show phone X': Показывает один номер телефона X('job/home/own') текущего пользователя.\n"
-          "'show all': Показывает все номера телефонов пользователя который выбран в данный момент.\n"
+          "'add X Y': Добавляет к записи пользователя X номер телефона Y.\n"
+          "'change X Y V': Заменяет для пользователя X номер телефона Y на номер телефона V.\n"
+          "'delete contact Х': Полностью удаляет контакт Х из записной книги.\n" 
+          "'show phones X': Показывает все номера телефонов пользователя X.\n"
+          "'show all': Показывает всех пользователей что есть в записной книге и их номера телефонов."
           "'good bye/close/exit': Выход из программы.\n"
           )
     while True:
-
+        print(book)
         user_text = input()
         command, user_data = refactor_user_text(user_text)
-        if command in ("create contact", "choice contact"):  # Создаем или выбираем контакт с которым будем работать.
-            new_record = get_functional(command)(user_data[0])
-        else:
-            try:
-                result = get_functional(command)(new_record, user_data)
-            except UnboundLocalError:
-                print("Вы не выбрали пользователя для взаимодействия.\n"
-                      "Создайте нового с помощью команды 'create contact X' или выберите существующего с помощью 'choice contact X'")
-                continue
-            if type(result) is str:
+        if command not in commands:
+            print(f"Неизвестная команда '{command}'")
+            continue
+        main_record = book.setdefault(user_data[0], Record(user_data[0])) if user_data else None
+        result = get_functional(command)(main_record, user_data[1:])
+        if result:
+            if type(result) is list:
+                print(*result, sep="\n")
+            else:
                 print(result)
-        try:
-            print(f"На данный момент мы работает с пользователем {new_record.name.value}.\n"
-                  f"Не забывайте сохранять данные с помощью команды save в адресную книгу перед сменой пользователя или завершением работы, иначе данные будут "
-                  f"утеряны.")
-        except AttributeError:
-            print(f"Не удалось найти пользователя {user_data[0]}. Возможно вы не сохранили данные или не корректно ввели имя.")
+
+
 
 
 @input_error
 def get_functional(command: str):
+    """Получение сигнатуры"""
     signature = commands[command]
     return signature
 
 
 @input_error
 def refactor_user_text(user_text: str) -> list:
+    """Обработка введенного пользователем текста и разделение на понятные для программы части"""
+
     splited_text = user_text.split()
     # На случай если команда состоит из двух слов через пробел. Такие команды начинаются с нескольких конкретных слов.
-    if splited_text[0].lower() in ("show", "good", "create", "choice"):
+    if splited_text[0].lower() in ("show", "good", "delete"):
         return [" ".join(splited_text[:2]).lower(), splited_text[2:]]
     else:
-        return [splited_text[0].lower(), splited_text[1:]]
-
+        return [splited_text[0].lower(),  splited_text[1:]]
 
 @input_error
 def greeting(*_) -> str:
@@ -72,39 +67,36 @@ def greeting(*_) -> str:
 
 
 @input_error
-def create_new_contact(name):
-    return Record(name)
-
-
-@input_error
-def choice_user(name):
-    user = book[name]
-    return user
+def delete_contact(main_record: Record, *_):
+    del book[main_record.name.value]
 
 
 @input_error
 def add_number(new_record: Record, user_info: list):
-    new_record.add_or_change_phone(user_info[0], user_info[1])
+    new_record.add_phone(int(user_info[0]))
 
 
 @input_error
-def change(new_record: Record, user_info: list):
-    new_record.add_or_change_phone(user_info[0], user_info[1])
+def change_number(new_record: Record, user_info: list):
+    new_record.edit_phone(int(user_info[0]), int(user_info[1]))
+
+@input_error
+def delete_phone(main_record: Record, user_info: list):
+    main_record.delete_phone(int(user_info[0]))
 
 
 @input_error
-def save_to_book(new_record, *_):
-    book.add_record(new_record)
+def show_phones(new_record: Record, *_) -> str:
+    return f"{new_record.name.value} contact phone numbers are: {[v.value for v in new_record.phones]}"
 
 
 @input_error
-def show_one_user_number(new_record: Record, user_info: list) -> str:
-    return f"{user_info[0]} telephone number is: {new_record.phones.value[user_info[0]]}"
+def show_all(*_):
+    full_list = []
+    for contact_name, numbers in book.items():
+        full_list.append(f"{contact_name} содержит такие номера телефонов: {[v.value for v in numbers.phones]}")
+    return full_list
 
-
-@input_error
-def show_all(new_record: Record, *_) -> str:
-    return "\n".join(f"{new_record.name.value} contact: '{k}' phone number is: {v}" for k, v in new_record.phones.value.items())
 
 
 @input_error
@@ -112,14 +104,12 @@ def go_away(*_):
     exit("Bye. See you soon.")
 
 
-contacts = {'Stas': 50, 'Kate': 66}
 commands = {"hello": greeting,
-            "create contact": create_new_contact,
-            "choice contact": choice_user,
+            "delete contact": delete_contact,
             "add": add_number,
-            "change": change,
-            "save": save_to_book,
-            "show phone": show_one_user_number,
+            "change": change_number,
+            "delete phone": delete_phone,
+            "show phones": show_phones,
             "show all": show_all,
             "good bye": go_away,
             "close": go_away,
