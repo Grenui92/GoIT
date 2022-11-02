e4154e3a858aa90d6c8d8fe2c5493c3186601109
Grenui92 <107549723+Grenui92@users.noreply.github.com>
2022-11-02 17:08:22 +0200

    Изменения согласно рекомендациям

--- Autocheck/PCHw10/Homework/contact_book_classes.py
+++ Autocheck/PCHw10/Homework/contact_book_classes.py
@@ -1,84 +1,67 @@
 from collections import UserDict
 
-def try_error_decorator(func):
-    def inner(*user_data):
-        try:
-            return func(*user_data)
-        except IndexError:
-            print(f"Index Error with {user_data[1:]}")
-        except KeyError:
-            print(f"I cant find user with name '{user_data[1]}'")
-        except ValueError:
-            print(f"'{user_data[2]}' is not a phone number")
-    return inner
 
 
 class AdressBook(UserDict):
-
-    def add_record(self, user):
-        self.data[user.name.value] = user
+    pass
 
 
 class Record:
 
-    def __init__(self, name):
+    def __init__(self, name, phone=None, email=None):
         self.name = Name(name)
-        self.phones = Phone
-        self.emails = Emails
-
-    @try_error_decorator
-    def delete_phone(self, which):
-        del self.phones.value[which]
-
-    @try_error_decorator
-    def delete_email(self, which):
-        del self.emails.value[which]
-
-    @try_error_decorator
-    def add_or_change_phone(self, which, number):
-        self.phones.value[which] = int(number)
-
-    @try_error_decorator
-    def add_or_change_email(self, which, email):
-        self.emails.value[which] = email
+        self.phones = [Phone(phone)] if phone else []
 
 
-class Field(Record):
-    pass
+    def delete_phone(self, number):
+        for num, value in enumerate(self.phones):
+            if value.value == number:
+                self.phones.pop(num)
+                break
+        else:
+            print(f"I cant find {number} in user {self.name.value}")
 
-class Name(Field):
+    def edit_phone(self, old_number, new_number):
+        for num, values in enumerate(self.phones):
+            if values.value == old_number:
+                values.value = new_number
+                break
+        else:
+            print(f"I cant find {old_number} in user {self.name.value}")
+
+
+    def add_phone(self, number):
+        self.phones.append(Phone(number))
+
+
+
+
+class Field:
     def __init__(self, name):
         self.value = name
 
+
+class Name(Field):
+    pass
+
 class Phone(Field):
-    value = {}
-
-class Emails(Field):
-    value = {}
+    pass
 
 
 
-# book = AdressBook()
-# stas = Record('Stas')
-# stas.add_or_change_phone("job", "0500801492")
-# stas.add_or_change_email("job", "new_check@gmail.com")
-# stas.add_or_change_phone("home", "0500801492")
-# stas.add_or_change_phone("new", "asd")
-# stas.add_or_change_email("home", "new_check@gmail.com")
-# stas.delete_phone("job")
-# stas.delete_email("home")
-# book.add_record(stas)
-# dima = Record("Dima")
-# dima.add_or_change_phone("job", "0500801492")
-# dima.add_or_change_email("job", "new_check@gmail.com")
-# dima.add_or_change_phone("home", "0500801492")
-# dima.add_or_change_email("home", "new_check@gmail.com")
-# stas.delete_phone("home")
-# stas.delete_email("job")
-# book.add_record(dima)
-# print(book)
-# stas.delete_email("asd")
-# print(book["Stas"].name.value)
+
+book = AdressBook()
+stas = Record('Stas')
+stas.add_phone("555")
+stas.add_phone("asd")
+stas.add_phone("111")
+stas.delete_phone("asd")
+stas.edit_phone("555", "aaa")
+stas.edit_phone("qwe", "777")
+for cl in stas.phones:
+    print(cl.value)
+
+print(stas.phones)
 
 
 
